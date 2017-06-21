2544b49e74f423ebec02a02394a7e06d381e2171
Michael Tu <tuzhucheng@outlook.com>
2017-06-21 15:43:02 -0400

    SM Model thrift - record latency

--- src/main/thrift/client_performance.py
+++ src/main/thrift/client_performance.py
@@ -5,16 +5,19 @@ import sys
 import time
 sys.path.append('gen-py')
 
-from qa import QuestionAnswering
-
+import numpy as np
 from thrift import Thrift
 from thrift.transport import TSocket
 from thrift.transport import TTransport
 from thrift.protocol import TBinaryProtocol
 
+from qa import QuestionAnswering
+
 
 def query(client, question, answer):
-    return client.getScore(question, answer)
+    query_start_time = time.time()
+    client.getScore(question, answer)
+    return time.time() - query_start_time
 
 
 def main(questions, answers, repeat, threads):
@@ -38,13 +41,17 @@ def main(questions, answers, repeat, threads):
 
     start_time = time.time()
 
+    latencies = []
     for _ in range(repeat):
-        pool.starmap(query, zip(itertools.repeat(client), questions, answers))
+        latencies.extend(pool.starmap(query, zip(itertools.repeat(client), questions, answers)))
 
-    end_time = time.time()
-    elapsed_time = end_time - start_time
+    elapsed_time = time.time() - start_time
 
+    latencies = np.array(latencies)
     print('Throughput (QPS): ', repeat * len(questions) / elapsed_time)
+    print('Average Latency (ms): ', np.mean(latencies) * 1000)
+    print('Latency p50 (ms): ', np.percentile(latencies, 50) * 1000)
+    print('Latency p99 (ms): ', np.percentile(latencies, 99) * 1000)
 
     transport.close()
 
